6581caa816c988a86279f1f9eada75d5b9d48a84
SadredinYousefi <sadredinyousefi@gmail.com>
2023-04-07 15:31:59 +0430

    Creating folders and files

---
+++ app/server.js
@@ -0,0 +1,52 @@
+const { log } = require("console");
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const { allRoutes } = require("./router/router");
+module.exports = class Application {
+  #app = express();
+  #PORT;
+  #DB_URI;
+  constructor(PORT, DB_URI) {
+    this.#PORT = PORT;
+    this.#DB_URI = DB_URI
+    this.configApplication();
+    this.connectToDB();
+    this.createServer();
+    this.createRoute();
+    this.errorHandler();
+  }
+  configApplication() {
+    this.#app.use(express.json);
+    this.#app.use(express.urlencoded({ extended: true }));
+    this.#app.use(express.static(path.join(__dirname, "..", "public")));
+  }
+  connectToDB() {
+    mongoose.connect(this.#DB_URI)
+  }
+  createServer() {
+    const http = require("http");
+    http.createServer(this.#app).listen(this.#PORT, () => {
+      console.log(`http://localhost:${this.#PORT}`);
+    });
+  }
+  createRoute() {
+    this.#app.use(allRoutes)
+  }
+  errorHandler() {
+    this.#app.use((req, res, next) => {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: "Page Not Found",
+      });
+    });
+    this.#app.use((error, req, res, next) => {
+      const statusCode = error.status || 500;
+      const msg = error.message || "Internal error";
+      return res.status(statusCode).json({
+        statusCode,
+        msg,
+      });
+    });
+  }
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+const Application = require("./app/server") ;
+
+new Application(5000 , "mongodb+srv://sadredinyousefi:13781999@myapp.oywtoen.mongodb.net/?retryWrites=true&w=majority")
