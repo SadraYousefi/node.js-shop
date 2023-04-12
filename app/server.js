d5728b9183e2b2bcdf71f0ddd126e12929c8a279
SadredinYousefi <sadredinyousefi@gmail.com>
2023-04-12 12:32:59 +0430

    refactor mongoConnection and now we are using morgan

--- app/server.js
+++ app/server.js
@@ -3,13 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const { allRoutes } = require("./router/router");
+const morgan = require("morgan");
 module.exports = class Application {
   #app = express();
   #PORT;
   #DB_URI;
   constructor(PORT, DB_URI) {
     this.#PORT = PORT;
-    this.#DB_URI = DB_URI
+    this.#DB_URI = DB_URI;
     this.configApplication();
     this.connectToDB();
     this.createServer();
@@ -17,25 +18,36 @@ module.exports = class Application {
     this.errorHandler();
   }
   configApplication() {
+    this.#app.use(morgan("dev"));
     this.#app.use(express.json());
     this.#app.use(express.urlencoded({ extended: true }));
     this.#app.use(express.static(path.join(__dirname, "..", "public")));
   }
   connectToDB() {
-    mongoose.connect(this.#DB_URI)
+    mongoose.connect(this.#DB_URI);
+    mongoose.connection.on("connected", () => {
+      console.log("Mongoose connected to DB");
+    });
+    mongoose.connection.on("disconnected ", () => {
+      console.log("mongoose disconnected");
+    });
+    process.on("SIGINT" , async ()=> { 
+      await mongoose.connection.close()
+      process.exit(0)
+    })
   }
   createServer() {
     try {
       const http = require("http");
-      http.createServer(this.#app ).listen(this.#PORT, () => {
-        console.log(`http://localhost:${this.#PORT}`);
+      http.createServer(this.#app).listen(this.#PORT, () => {
+        console.log(`Server is running on : http://localhost:${this.#PORT}`);
       });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
   createRoute() {
-    this.#app.use(allRoutes)
+    this.#app.use(allRoutes);
   }
   errorHandler() {
     this.#app.use((req, res, next) => {
